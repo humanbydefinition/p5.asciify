710bdf7b931180c4554e4c859ed1653f5b893de8
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-07-23 00:30:05 +0200

    Fixed ascii rotation functionality.
    Now there shouldn't be any more artifacts on corners when rotating to 45 degrees for example, also keeping in mind any character/background color mode and the invert mode.

--- src/constants.js
+++ src/constants.js
@@ -51,19 +51,19 @@ class P5AsciifyConstants {
                                         void main() {
                                             vec2 adjustedCoord = (gl_FragCoord.xy - u_gridOffsetDimensions) / u_gridPixelDimensions;
 
-                                            if (adjustedCoord.x < 0.0 || adjustedCoord.x > 1.0 || adjustedCoord.y < 0.0 || adjustedCoord.y > 1.0) {
-                                                fragColor = vec4(u_backgroundColor, 1.0);
+                                            if(adjustedCoord.x < 0.0f || adjustedCoord.x > 1.0f || adjustedCoord.y < 0.0f || adjustedCoord.y > 1.0f) {
+                                                fragColor = vec4(u_backgroundColor, 1.0f);
                                                 return;
                                             }
 
                                             // Calculate the grid coordinate
                                             vec2 gridCoord = adjustedCoord * u_gridCellDimensions;
                                             vec2 cellCoord = floor(gridCoord);
-                                            vec2 centerCoord = cellCoord + vec2(0.5);
+                                            vec2 centerCoord = cellCoord + vec2(0.5f);
                                             vec2 baseCoord = centerCoord / u_gridCellDimensions;
 
                                             vec4 sketchColor = texture(u_sketchTexture, baseCoord);
-                                            float brightness = dot(sketchColor.rgb, vec3(0.299, 0.587, 0.114));
+                                            float brightness = dot(sketchColor.rgb, vec3(0.299f, 0.587f, 0.114f));
 
                                             // Map the brightness to a character index
                                             int charIndex = int(brightness * float(u_totalChars));
@@ -75,25 +75,25 @@ class P5AsciifyConstants {
 
                                             // Calculate the texture coordinate of the character in the charset texture
                                             vec2 charCoord = vec2(float(charCol) / u_charsetCols, float(charRow) / u_charsetRows);
-                                            vec2 fractionalPart = fract(gridCoord) - 0.5; // Center fractional part around (0,0) for rotation
+                                            vec2 fractionalPart = fract(gridCoord) - 0.5f; // Center fractional part around (0,0) for rotation
                                             fractionalPart = rotate2D(u_rotationAngle) * fractionalPart; // Rotate fractional part
-                                            fractionalPart += 0.5; // Move back to original coordinate space
+                                            fractionalPart += 0.5f; // Move back to original coordinate space
 
                                             // Calculate the texture coordinates
                                             vec2 cellMin = charCoord;
-                                            vec2 cellMax = charCoord + vec2(1.0 / u_charsetCols, 1.0 / u_charsetRows);
-                                            vec2 texCoord = charCoord + fractionalPart * vec2(1.0 / u_charsetCols, 1.0 / u_charsetRows);
+                                            vec2 cellMax = charCoord + vec2(1.0f / u_charsetCols, 1.0f / u_charsetRows);
+                                            vec2 texCoord = charCoord + fractionalPart * vec2(1.0f / u_charsetCols, 1.0f / u_charsetRows);
 
                                             // Determine if the texture coordinate is within the cell boundaries
                                             bool outsideBounds = any(lessThan(texCoord, cellMin)) || any(greaterThan(texCoord, cellMax));
 
                                             // Get the color of the character from the charset texture or use the background color if outside bounds
-                                            vec4 charColor = outsideBounds ? vec4(u_backgroundColor, 1.0) : texture(u_characterTexture, texCoord);
+                                            vec4 charColor = outsideBounds ? vec4(u_backgroundColor, 1.0f) : texture(u_characterTexture, texCoord);
 
                                             // If the inversion mode is enabled, invert the character color
-                                            if (u_invertMode == 1) {
-                                                charColor.a = 1.0 - charColor.a;
-                                                charColor.rgb = vec3(1.0);
+                                            if(u_invertMode == 1) {
+                                                charColor.a = 1.0f - charColor.a;
+                                                charColor.rgb = vec3(1.0f);
                                             }
 
                                             // Calculate the final color of the character
@@ -101,10 +101,17 @@ class P5AsciifyConstants {
 
                                             // If the background color mode is 0, mix the sketch color and the final color based on the character's alpha value
                                             // Otherwise, mix the background color and the final color based on the character's alpha value
-                                            if (u_backgroundColorMode == 0) {
-                                                fragColor = mix(vec4(sketchColor.rgb, 1.0), finalColor, charColor.a);
+                                            if(u_backgroundColorMode == 0) {
+                                                fragColor = mix(vec4(sketchColor.rgb, 1.0f), finalColor, charColor.a);
                                             } else {
-                                                fragColor = mix(vec4(u_backgroundColor, 1.0), finalColor, charColor.a);
+                                                fragColor = mix(vec4(u_backgroundColor, 1.0f), finalColor, charColor.a);
+                                            }
+
+                                            // Override final color with background color for out-of-bounds areas due to rotation
+                                            if (outsideBounds) {
+                                            fragColor = (u_backgroundColorMode == 0) 
+                                                        ? (u_invertMode == 1 ? (u_characterColorMode == 0 ? vec4(sketchColor.rgb, 1.0f) : vec4(u_characterColor, 1.0f)) : vec4(sketchColor.rgb, 1.0f)) 
+                                                        : (u_invertMode == 1 ? (u_characterColorMode == 0 ? vec4(sketchColor.rgb, 1.0f) : vec4(u_characterColor, 1.0f)) : vec4(u_backgroundColor, 1.0f));
                                             }
                                         }`;
 
