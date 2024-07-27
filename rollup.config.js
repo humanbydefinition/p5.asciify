381712253c678e966dd30c5cdd802496a74390f3
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-07-28 01:34:37 +0200

    Updated to only bundle esm as standard

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,16 @@ import terser from '@rollup/plugin-terser';
 import pkg from './package.json' assert { type: 'json' };
 
 export default [
-    // browser-friendly UMD build
+    // ES module (for bundlers) build.
     {
         input: 'src/index.js',
         output: [
-            {
-                name: 'p5.asciify',
-                file: pkg.browser,
-                format: 'umd'
-            },
-            {
-                name: 'p5.asciify',
-                file: 'dist/p5.asciify.umd.min.js',
-                format: 'umd',
-                plugins: [terser()]
-            }
+            { file: 'dist/p5.asciify.js', format: 'es' },
+            { file: 'dist/p5.asciify.min.js', format: 'es', plugins: [terser()] }
         ],
         plugins: [
             resolve(), // so Rollup can find dependencies
             commonjs() // so Rollup can convert dependencies to ES modules
-        ]
-    },
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    {
-        input: 'src/index.js',
-        output: [
-            { file: pkg.main, format: 'cjs' },
-            { file: pkg.module, format: 'es' },
-            { file: 'dist/p5.asciify.cjs.min.js', format: 'cjs', plugins: [terser()] },
-            { file: 'dist/p5.asciify.esm.min.js', format: 'es', plugins: [terser()] }
         ]
     }
 ];
