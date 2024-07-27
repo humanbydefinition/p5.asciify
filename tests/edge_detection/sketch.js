9e7e1e20a7955f382622a698fe6673f2860a6de6
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-07-27 13:35:25 +0200

    Refactored edge detection test

--- tests/edge_detection/sketch.js
+++ tests/edge_detection/sketch.js
@@ -10,80 +10,53 @@ function setup() {
     sketchFramebuffer = createFramebuffer({ format: FLOAT });
 
     setAsciiOptions({ // These are the default options, you can change them as needed in preload(), setup() or draw()
-        enabled: true,
-        characters: ' .:-=+*#%@',
-        fontSize: 8,
-        characterColor: "#ff0000",
-        characterColorMode: 1,
-        backgroundColor: "#000000",
-        backgroundColorMode: 1,
-        invertMode: false,
+        common: {
+            fontSize: 32,
+        },
+        brightness: {
+            enabled: true,
+            characters: "0123456789",
+            characterColor: "#ffffff",
+            characterColorMode: 0,
+            backgroundColor: "#000000",
+            backgroundColorMode: 1,
+            invertMode: false,
+            rotationAngle: 90
+        },
+        edge: {
+            enabled: true,
+            characters: "-/|\\-/|\\",
+            characterColor: '#ffffff',
+            characterColorMode: 1,
+            backgroundColor: '#000000',
+            backgroundColorMode: 1,
+            invertMode: false,
+            sobelThreshold: 0.5,
+            sampleThreshold: 1,
+            rotationAngle: 0,
+        }
     });
-
-	// Add pre-effects, which get applied before the ASCII conversion
-    distortionEffect = addAsciiEffect('pre', 'distortion', { frequency: 5.0, amplitude: 0.4 });
-    kaleidoscopeEffect = addAsciiEffect('pre', 'kaleidoscope', { segments: 4, angle: 0 });
-
-    for (let i = 0; i < maxRectangles; i++) rectangles.push(new Rectangle());
 }
 
 function draw() {
     sketchFramebuffer.begin();
     background(0);
-    noStroke();
+	fill(255);
 
-    rectangles = rectangles.filter(rect => {
-        rect.update();
-        rect.display();
-        return rect.size > 0;
-    });
-
-    while (rectangles.length < maxRectangles) rectangles.push(new Rectangle());
+	push();
+	rotateX(radians(frameCount * 1));
+	rotateZ(radians(frameCount));
+	directionalLight(255, 255, 255, 0, 0, -1);
+	box(800, 400, 400);
+	pop();
 
     sketchFramebuffer.end();
     background(0);
     image(sketchFramebuffer, -windowWidth / 2, -windowHeight / 2);
 
-	// Change the kaleidoscope angle direction randomly if the cooldown is over and a random chance is met
-    if (cooldownCounter-- <= 0 && random() < 0.01) {
-        angleDirection *= -1; 
-        cooldownCounter = 120;
-    }
-
-    kaleidoscopeEffect.angle += angleDirection; // Rotate the angle of the kaleidoscope effect
+    setAsciiOptions({ });
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-}
-
-class Rectangle {
-    constructor() {
-        this.x = random(-windowWidth / 2, windowWidth / 2);
-        this.y = random(-windowHeight / 2, windowHeight / 2);
-        this.size = 0;
-        this.maxSize = random(50, 200);
-        this.growing = true;
-        this.color = color(random(255), random(255), random(255));
-    }
-
-    update() {
-        if (this.growing) {
-            this.size = min(this.size + 3, this.maxSize);
-            this.growing = this.size < this.maxSize;
-        } else {
-            this.size = max(this.size - 3, 0);
-            if (this.size === 0) return true;
-        }
-
-        if (!this.growing && random() < 0.01) this.growing = true;
-        if (random() < 0.01) this.growing = false;
-
-        return false;
-    }
-
-    display() {
-        fill(this.color);
-        rect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
-    }
 }
