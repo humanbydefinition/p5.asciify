0caaa44ae5c9bfeac185c8deb3ac01643c6933a1
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-07-28 01:55:09 +0200

    Added docstrings

--- src/characterset.js
+++ src/characterset.js
@@ -1,5 +1,18 @@
+/**
+ * @module characterset
+ * @description
+ * A class that represents a character set for the P5Asciify library.
+ * It is responsible for maintaining a texture that contains all the characters in the character set.
+ */
 class P5AsciifyCharacterSet {
 
+    /**
+     * Sets up the character set with a specified font, characters, and font size.
+     * @param {Object} options - The setup options.
+     * @param {Object} options.font - The font object to use.
+     * @param {string} options.characters - The string of characters to include in the character set.
+     * @param {number} options.fontSize - The font size to use.
+     */
     setup({ font, characters, fontSize }) {
         this.font = font;
         this.characters = Array.from(characters);
@@ -7,6 +20,9 @@ class P5AsciifyCharacterSet {
         this.reset();
     }
 
+    /**
+     * Resets the character set by reloading the glyphs and creating a new texture.
+     */
     reset() {
         this.glyphs = Object.values(this.font.font.glyphs.glyphs);
         this.glyphs = this.glyphs.filter(glyph => glyph.unicode !== undefined);
@@ -16,6 +32,11 @@ class P5AsciifyCharacterSet {
         this.createTexture({ fontSize: 512 });
     }
 
+    /**
+     * Calculates the maximum dimensions of glyphs in the whole font for a given font size.
+     * @param {number} fontSize - The font size to use for calculations.
+     * @returns {Object} An object containing the maximum width and height of the glyphs.
+     */
     getMaxGlyphDimensions(fontSize) {
         return this.glyphs.reduce((maxDims, glyph) => {
             const bounds = glyph.getPath(0, 0, fontSize).getBoundingBox();
@@ -26,21 +47,40 @@ class P5AsciifyCharacterSet {
         }, { width: 0, height: 0 });
     }
 
+    /**
+     * Sets the font object and resets the character set.
+     * @param {Object} font - The new font object.
+     */
     setFontObject(font) {
         this.font = font;
         this.reset();
     }
 
+    /**
+     * Sets the characters to be used in the character set and creates a new texture.
+     * @param {string} characters - The string of characters to set.
+     */
     setCharacterSet(characters) {
         this.characters = Array.from(characters);
         this.createTexture({ fontSize: 512 });
     }
 
+    /**
+     * Sets a specific character at a given index in the character set and updates the texture.
+     * @param {Object} options - The character options.
+     * @param {string} options.character - The character to set.
+     * @param {number} options.index - The index at which to set the character.
+     */
     setCharacter({ character, index }) {
         this.characters[index] = character;
         this.createTexture({ fontSize: 512 });
     }
 
+    /**
+     * Returns an array of characters that are not supported by the current font.
+     * @param {string} characters - The string of characters to check.
+     * @returns {string[]} An array of unsupported characters.
+     */
     getUnsupportedCharacters(characters) {
         const badCharacters = new Set();
 
@@ -55,17 +95,25 @@ class P5AsciifyCharacterSet {
         return Array.from(badCharacters);
     }
 
+    /**
+     * Sets the font size and updates the maximum glyph dimensions.
+     * @param {number} fontSize - The new font size.
+     */
     setFontSize(fontSize) {
         this.fontSize = fontSize;
         this.maxGlyphDimensions = this.getMaxGlyphDimensions(this.fontSize);
     }
 
+    /**
+     * Creates a texture containing all characters in the character set, arranged in a 2d grid.
+     * @param {Object} options - The texture creation options.
+     * @param {number} options.fontSize - The font size to use for rendering the texture.
+     */
     createTexture({ fontSize }) {
-        // Calculate the number of columns and rows for the texture based on the number of characters
         this.charsetCols = Math.ceil(Math.sqrt(this.characters.length));
         this.charsetRows = Math.ceil(this.characters.length / this.charsetCols);
 
-        let dimensions = this.getMaxGlyphDimensions(fontSize); // Calculate the dimensions of the texture
+        let dimensions = this.getMaxGlyphDimensions(fontSize);
 
         if (!this.texture) {
             this.texture = createFramebuffer({ format: FLOAT, width: dimensions.width * this.charsetCols, height: dimensions.height * this.charsetRows });
