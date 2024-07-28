03c273ea1ddb1b4d8f18f026dfc344a5f5a7f2ab
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-07-28 14:36:34 +0200

    Updated edge detection test

--- tests/edge_detection/sketch.js
+++ tests/edge_detection/sketch.js
@@ -1,34 +1,22 @@
+/**
+ * This test checks if the ascii edge detection works correctly on a 3D object.
+ */
+
 let sketchFramebuffer;
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL); // WebGL mode is required currently
     sketchFramebuffer = createFramebuffer({ format: FLOAT });
 
-    setAsciiOptions({ // These are the default options, you can change them as needed in preload(), setup() or draw()
+    setAsciiOptions({ 
         common: {
             fontSize: 32,
         },
-        brightness: {
-            enabled: true,
-            characters: "0123456789",
-            characterColor: "#ffffff",
-            characterColorMode: 0,
-            backgroundColor: "#000000",
-            backgroundColorMode: 1,
-            invertMode: false,
-            rotationAngle: 90
-        },
         edge: {
             enabled: true,
-            characters: "-/|\\-/|\\",
-            characterColor: '#ffffff',
             characterColorMode: 1,
-            backgroundColor: '#000000',
-            backgroundColorMode: 1,
-            invertMode: false,
             sobelThreshold: 0.5,
-            sampleThreshold: 1,
-            rotationAngle: 0,
+            sampleThreshold: 32,
         }
     });
 }
@@ -37,19 +25,13 @@ function draw() {
     sketchFramebuffer.begin();
     background(0);
 	fill(255);
-
-	push();
 	rotateX(radians(frameCount * 1));
 	rotateZ(radians(frameCount));
 	directionalLight(255, 255, 255, 0, 0, -1);
 	box(800, 400, 400);
-	pop();
-
     sketchFramebuffer.end();
-    background(0);
-    image(sketchFramebuffer, -windowWidth / 2, -windowHeight / 2);
 
-    setAsciiOptions({ });
+    image(sketchFramebuffer, -windowWidth / 2, -windowHeight / 2);
 }
 
 function windowResized() {
