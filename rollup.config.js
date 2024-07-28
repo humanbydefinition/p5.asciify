42030ce053b139ac110ca2a768f982082649866e
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-07-28 05:10:53 +0200

    Added rollup-plugin-glslify

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-import pkg from './package.json' assert { type: 'json' };
+import glslify from 'rollup-plugin-glslify';
 
 export default [
-    // ES module (for bundlers) build.
     {
         input: 'src/index.js',
         output: [
@@ -12,6 +11,7 @@ export default [
             { file: 'dist/p5.asciify.min.js', format: 'es', plugins: [terser()] }
         ],
         plugins: [
+            glslify(),
             resolve(), // so Rollup can find dependencies
             commonjs() // so Rollup can convert dependencies to ES modules
         ]
