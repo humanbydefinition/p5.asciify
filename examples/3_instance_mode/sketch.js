899fb5cb6a6053de2617ea1e4dd1b9e478539759
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-08-22 22:00:34 +0200

    Added comments

--- examples/3_instance_mode/sketch.js
+++ examples/3_instance_mode/sketch.js
@@ -1,6 +1,6 @@
 const theSketch = (sketch) => {
 
-    p5asciify.instance(sketch);
+    p5asciify.instance(sketch); // Pass the p5 instance to the p5asciify library
 
     sketch.preload = () => {
     };
@@ -8,7 +8,7 @@ const theSketch = (sketch) => {
     sketch.setup = () => {
         sketch.createCanvas(800, 800, sketch.WEBGL);
 
-        sketch.setAsciiOptions({
+        sketch.setAsciiOptions({ // Put "sketch." or whatever your p5 instance is called before the functions that p5.asciify provides
             brightness: {
                 enabled: true
             }
@@ -16,7 +16,6 @@ const theSketch = (sketch) => {
     };
 
     sketch.draw = () => {
-
         sketch.push();
         sketch.background(0);
         sketch.fill(255);
