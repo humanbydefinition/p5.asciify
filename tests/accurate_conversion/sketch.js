1203dd4951fb1aee039b3be024ca527415e34cab
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2024-11-16 22:39:19 +0100

    Refactored accurate conversion test

--- tests/accurate_conversion/sketch.js
+++ tests/accurate_conversion/sketch.js
@@ -7,7 +7,7 @@ let sketchFramebuffer;
 let img;
 
 function preload() {
-    img = loadImage('brutalist-high-rise-building.jpeg');
+    img = loadImage('brutalist-high-rise-building.jpeg'); // forgot where it's from, but definitely CC0
 }
 
 function setup() {
@@ -20,6 +20,7 @@ function setup() {
         },
         ascii: {
             enabled: true,
+            // All characters available in the default font `UrsaFont` provided by the library
             characters: ` .‼╨\`',-_ú:░+;╘╙╤╥!"/=\^i~±âñ◘─├┬┴()*<>l|ÖÜèë←↑→↓♪?fr{}°²¶ÄÅÆÉíüƒΘΦφⁿ₧∙∞┌┘╒╓╪╫■□♀LT[]cjptvxzÑ∟7qyä│┤║╕╖╡╢╣Feo«»½απ↨≤≥╗☼1au•↔4CIYZks┼╔╚╞╟╩▄█▌▐$3gn£à▬◙%2EJSmw¡ª¼↕≈╜╝▲►◄♂59hå♠♦PVXbd¥6@GKç♣8ADOUÇáéó▀▒♫ê⌂#0HR§ºBQ═╠╦╬¢¬µ¿ï÷⌐&MNW♥ìîτ└▼ε∩╧▓☺☻ÿùûòöæô≡ßΓΣσ⌠⌡┐╛○·Ωδ√`,
             renderMode: 'accurate',
             characterColorMode: 0,
@@ -29,8 +30,19 @@ function setup() {
 }
 
 function draw() {
-
     sketchFramebuffer.begin();
+
+    background(0);
+    
+    push();
+    translate(100, 0, 0);
+	fill(255);
+	rotateX(radians(frameCount * 3));
+	rotateZ(radians(frameCount));
+	directionalLight(255, 255, 255, 0, 0, -1);
+	box(600, 80, 80);
+    pop();
+
     image(img, -windowWidth / 2, -windowHeight / 2);
     sketchFramebuffer.end();
 
