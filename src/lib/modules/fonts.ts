fa41c8817c0d5439f9ec12e8e6be30ebb4f2cce6
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-12 20:13:57 +0100

    Removed redundant promise

--- src/lib/modules/fonts.ts
+++ src/lib/modules/fonts.ts
@@ -7,33 +7,29 @@ export function registerFontMethods(p5asciify: P5Asciifier): void {
 
     p5.prototype.preloadAsciiFont = function (this: p5): void {
         this._incrementPreload();
-        this.loadAsciiFont(URSAFONT_BASE64)
-            .catch((error) => {
-                throw new P5AsciifyError(`preloadAsciiFont() | Failed to load default font: ${error.message}`);
-            });
+        this.loadAsciiFont(URSAFONT_BASE64);
     };
 
-    p5.prototype.loadAsciiFont = function (this: p5, font: string | p5.Font): Promise<void> {
-        return new Promise((resolve, reject) => {
-            const setFont = async (loadedFont: p5.Font) => {
-                p5asciify.font = loadedFont;
-                this._decrementPreload();
-                p5asciify.emit('fontUpdated', {});
-                resolve();
-            };
-
-            if (typeof font === 'string') {
-                this.loadFont(
-                    font,
-                    (loadedFont: p5.Font) => { setFont(loadedFont); },
-                    () => { reject(new P5AsciifyError(`loadAsciiFont() | Failed to load font from path: '${font}'`)); }
-                );
-            } else if (font instanceof p5.Font) {
-                setFont(font);
-            } else {
-                reject(new P5AsciifyError('loadAsciiFont() | Invalid font parameter. Expected a path, base64 string, or p5.Font object.'));
-            }
-        });
+    p5.prototype.loadAsciiFont = function (this: p5, font: string | p5.Font): void {
+        const setFont = (loadedFont: p5.Font) => {
+            p5asciify.font = loadedFont;
+            this._decrementPreload();
+            p5asciify.emit('fontUpdated', {});
+        };
+    
+        if (typeof font === 'string') {
+            this.loadFont(
+                font,
+                (loadedFont: p5.Font) => { setFont(loadedFont); },
+                () => { 
+                    throw new P5AsciifyError(`loadAsciiFont() | Failed to load font from path: '${font}'`);
+                }
+            );
+        } else if (font instanceof p5.Font) {
+            setFont(font);
+        } else {
+            throw new P5AsciifyError('loadAsciiFont() | Invalid font parameter. Expected a path, base64 string, or p5.Font object.');
+        }
     };
 
     p5.prototype.registerMethod("beforePreload", p5.prototype.preloadAsciiFont);
