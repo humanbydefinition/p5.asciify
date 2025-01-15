da16b215ec814ba4468d61075e30e1454ac52f0a
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-16 00:35:39 +0100

    Fixed `AsciiRendererOptions` typing

--- src/lib/renderers/types.ts
+++ src/lib/renderers/types.ts
@@ -3,10 +3,10 @@ import p5 from 'p5';
 export interface AsciiRendererOptions {
     enabled: boolean;
     characters?: string;
-    characterColor: string | p5.Color;
-    characterColorMode: number;
-    backgroundColor: string | p5.Color;
-    backgroundColorMode: number;
+    characterColor?: string | p5.Color;
+    characterColorMode?: number;
+    backgroundColor?: string | p5.Color;
+    backgroundColorMode?: number;
     invertMode: boolean;
     rotationAngle: number;
 }
