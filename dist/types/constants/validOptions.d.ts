689e77cf2d0c89a221120aefad93bfc985d08bb8
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-15 21:38:24 +0100

    Removed unused VALID_OPTIONS and VALID_RENDER_MODES constants

--- dist/types/constants/validOptions.d.ts
+++ dist/types/constants/validOptions.d.ts
@@ -1,6 +1,3 @@
-export declare const VALID_OPTIONS: string[];
-export declare const VALID_RENDER_MODES: string[];
-export declare const VALID_GRADIENT_NAMES: string[];
 export declare const FONT_SIZE_LIMITS: {
     MIN: number;
     MAX: number;
