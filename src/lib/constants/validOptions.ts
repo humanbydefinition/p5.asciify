189a5519cf571cd2e8ef6c2241856b52326e39c8
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-15 21:37:58 +0100

    Removed unused constants

--- src/lib/constants/validOptions.ts
+++ src/lib/constants/validOptions.ts
@@ -1,27 +1,3 @@
-export const VALID_OPTIONS = [
-    "common",
-    "edge",
-    "ascii",
-    "gradient",
-    "custom",
-    "text"
-];
-
-export const VALID_RENDER_MODES = [
-    'brightness',
-    'accurate',
-    'custom'
-];
-
-export const VALID_GRADIENT_NAMES = [
-    "conical",
-    "linear",
-    "noise",
-    "radial",
-    "spiral",
-    "zigzag",
-];
-
 export const FONT_SIZE_LIMITS = {
     MIN: 1,
     MAX: 128,
