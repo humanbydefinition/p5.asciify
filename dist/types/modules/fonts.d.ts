202b40a834f560485fbf03fae25fe97e3d3d6dc3
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-18 08:23:16 +0100

    Updated distributables

--- dist/types/modules/fonts.d.ts
+++
@@ -1,5 +0,0 @@
-import { P5Asciifier } from '../Asciifier';
-/**
- * Registers font-related methods to p5.js: preloads UrsaFont and adds loadAsciiFont().
- */
-export declare function registerFontMethods(p5asciify: P5Asciifier): void;
