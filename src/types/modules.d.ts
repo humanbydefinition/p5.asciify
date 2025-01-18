285fe9d8e61d90176fc179d1dcd530c5c14eed75
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-18 07:38:06 +0100

    Add type declarations for unrecognized asset files and update tsconfig

--- src/types/modules.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Used to declare file imports that are not recognized by TypeScript.
- * This way, we can import files like .vert, .frag, and .txt files without any linting errors.
- */
-declare module '*.txt?raw' {
-    const content: string;
-    export default content;
-}
