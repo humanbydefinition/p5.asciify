b2dba957144146525b18a4ed11ac2795c5534823
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-18 08:26:26 +0100

    Fully removed `modules` folder and moved leftovers into `index.ts`.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
+import p5 from 'p5';
 import { P5Asciifier } from './Asciifier';
-
-import { registerSetupMethods } from './modules/setup';
-import { registerRenderingMethods } from './modules/rendering';
+import { validateSetup } from './validators/SetupValidator';
+import URSAFONT_BASE64 from './assets/fonts/ursafont_base64.txt?raw';
 
 /**
  * The main instance of the p5.asciify library, which is used to access all of the library's functionality.
@@ -14,9 +14,56 @@ if (typeof window !== 'undefined') {
   window.p5asciify = p5asciify;
 }
 
-// Register library methods to extend the p5 instance
-registerSetupMethods(p5asciify);
-registerRenderingMethods(p5asciify);
+/**
+     * Add `setupAsciify` function to p5 instance, which can be overridden by the user.
+     * This function is called after the p5.asciify setup has been completed.
+     */
+p5.prototype.setupAsciify = function(): void {};
+
+/**
+ * Extend the p5.asciify instance to the p5 instance and run the p5.asciify init method
+ */
+p5.prototype.registerMethod('init', function(this: p5) {
+    p5asciify.instance(this, false);
+
+    this._incrementPreload();
+    p5asciify.font = URSAFONT_BASE64;
+});
+
+/**
+ * After the user's setup function is finished, run the p5.asciify setup
+ */
+p5.prototype.registerMethod('afterSetup', function(this: p5) {
+    validateSetup(this);
+    p5asciify.setup();
+    this.setupAsciify();
+});
+
+/**
+ * Add `drawAsciify` function to p5 instance, which can be overridden by the user.
+ * This function is called after the p5.asciify draw has been completed.
+ */
+p5.prototype.drawAsciify = function (): void { };
+
+/**
+ * Adds a pre-draw method to the p5.js instance which wraps the user draw loop in a framebuffer.
+ */
+p5.prototype.registerMethod("pre", function (this: p5): void {
+  p5asciify.sketchFramebuffer.begin();
+  this.clear();
+  this.push();
+});
+
+/**
+ * Adds a post-draw method to the p5.js instance which ends the framebuffer and calls the asciify method.
+ */
+p5.prototype.registerMethod("post", function (this: p5): void {
+  this.pop();
+  p5asciify.sketchFramebuffer.end();
+
+  p5asciify.rendererManager.render(p5asciify.sketchFramebuffer, p5asciify.borderColor);
+  this.drawAsciify();
+});
 
 // Exports
 export { P5Asciifier } from './Asciifier';
