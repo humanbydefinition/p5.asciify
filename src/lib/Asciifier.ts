2eef0f93be0b07a27bd830cf8224669b9f9d1a6b
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-18 19:46:00 +0100

    test changes for TypeDoc

--- src/lib/Asciifier.ts
+++ src/lib/Asciifier.ts
@@ -47,7 +47,10 @@ export class P5Asciifier {
     /** The font to use for the ASCII rendering. */
     private _font!: p5.Font;
 
-    /** The background color to use for the ASCII rendering for the offset space, not occupied by the centered ASCII grid. */
+    /** 
+     * The background color to use for the ASCII rendering for the offset space, not occupied by the centered ASCII grid.
+     * @default {@link DEFAULT_BACKGROUND_COLOR}
+     */
     private _backgroundColor: string | p5.Color | [number, number?, number?, number?] = DEFAULT_BACKGROUND_COLOR;
 
     /** The font size to use for the ASCII rendering. */
