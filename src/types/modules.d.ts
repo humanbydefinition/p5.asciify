ef69471896176b715d22676be48fb89074d785cc
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-18 04:48:55 +0100

    Remove unused module declarations for .vert and .frag file types

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -2,16 +2,6 @@
  * Used to declare file imports that are not recognized by TypeScript.
  * This way, we can import files like .vert, .frag, and .txt files without any linting errors.
  */
-declare module '*.vert' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.frag' {
-    const content: string;
-    export default content;
-}
-
 declare module '*.txt?raw' {
     const content: string;
     export default content;
