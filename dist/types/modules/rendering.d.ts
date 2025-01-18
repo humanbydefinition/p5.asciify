684c42a26b3ef330a81bb95ce1bf04e9bf3cd5fc
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-01-18 08:30:24 +0100

    Updated distributables

--- dist/types/modules/rendering.d.ts
+++
@@ -1,5 +0,0 @@
-import { P5Asciifier } from '../Asciifier';
-/**
- * Registers the rendering methods to the p5.js instance.
- */
-export declare function registerRenderingMethods(p5asciify: P5Asciifier): void;
--- dist/types/modules/setup.d.ts
+++
@@ -1,5 +0,0 @@
-import { P5Asciifier } from '../Asciifier';
-/**
- * Registers setup methods for the p5.asciify library onto the p5 instance.
- */
-export declare function registerSetupMethods(p5asciify: P5Asciifier): void;
