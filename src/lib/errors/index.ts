72903d96d3952ed620f9a2161a4579d7cc8f2a03
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-06-07 22:51:16 +0200

    Refactor export of P5AsciifyErrorLevel in error index

--- src/lib/errors/index.ts
+++ src/lib/errors/index.ts
@@ -2,7 +2,7 @@ export { P5AsciifyError } from './AsciifyError';
 export { 
     P5AsciifyErrorHandler, 
     errorHandler, 
-    P5AsciifyErrorLevel as ErrorLevel, 
+    P5AsciifyErrorLevel, 
     type ErrorHandlerOptions,
     type P5AsciifyErrorInfo 
 } from './ErrorHandler';
