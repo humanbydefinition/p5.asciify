f71005d0aa1073a02882275ec09f9241fef4bd3d
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-06-21 01:48:01 +0200

    refactor: simplify FontGrid component and move font data to separate JSON file

--- docs/src/components/FontGrid/FontGrid.tsx
+++ docs/src/components/FontGrid/FontGrid.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import FontCard, { FontCardProps } from '../FontCard/FontCard';
 import styles from './FontGrid.module.css';
+import fonts from '@site/src/data/fonts.json';
 
-export default function FontGrid({ fonts }: { fonts: FontCardProps[] }) {
+export default function FontGrid() {
   return (
     <div className={styles.grid}>
-      {fonts.map((font, index) => (
+      {fonts.map((font: FontCardProps, index: number) => (
         <div key={index} className={styles.gridItem}>
           <FontCard
             name={font.name}
